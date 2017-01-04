9a99f9fe21fd74beb6d9a4cbd7f60ac161416731
WaOnEmperoR <themusketeer17@gmail.com>
2017-01-04 13:15:47 +0700

    Lupa Commit, ketinggalan

--- application/views/lupa_password.php
+++ application/views/lupa_password.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Aplikasi Monitoring</title>
+    <title>MITENSILAN | Forgot Password</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Avant">
     <meta name="author" content="The Red Team">
-    <link href="<?php echo base_url(); ?>assets/img/favicon.png" rel="SHORTCUT ICON" />
+    <link href="<?php echo base_url(); ?>assets/img/BPKP.png" rel="SHORTCUT ICON" />
 
     <!-- <link href="assets/less/styles.less" rel="stylesheet/less" media="all"> -->
     <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/styles.css">
@@ -27,7 +27,7 @@
 <div class="verticalcenter">
     <div class="login-logo">
         <a href="#">
-            <img src="<?php echo base_url(); ?>assets/img/logo_login.png">
+            <img src="<?php echo base_url(); ?>assets/img/Logo Mitensilan.png" height="100px" width="275px">
             <br>
             <h4 class="description">
                 Sistem Informasi Monitoring SDM
